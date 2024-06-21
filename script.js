540f49b82bce0d12cb28446196cc62ecc830441f
leonam paulino <leonampaulinoagro@gmail.com>
2024-06-20 22:11:49 -0300

    jogo2024

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
-var numbertofind = 0;
+var numbertofind = null;
 var attempts = 0;
 
 
 function refresh() {
-    numbertofind = Math.floor(Math.random() * 100) + 1; 
-    console.log(numbertofind); 
+    numbertofind = Math.floor(Math.random() * 100);
+    console.log(numbertofind);
+    document.getElementById('bet').disabled = false; 
+    document.getElementById('submit-button').disabled = false; 
+    attempts = 0; 
+    document.getElementById('log').value = ''; 
 }
 
 
 function verifyNumber() {
-    var bet = parseInt(document.getElementById('bet').value); 
+    var bet = document.getElementById('bet').value; 
+    var log = document.getElementById('log'); 
 
-   
-    if (isNaN(bet) || bet < 0 || bet >= 100) {
-        alert('Aposta inválida! Insira um número entre 0 e 99.');
+    if (numbertofind === null) {
+        log.value += `O jogo ainda não foi iniciado. Clique em "Sortear Número" para começar.\n`;
         return;
     }
 
-    attempts++; 
+   
+    var betNumber = parseInt(bet);
+    if (isNaN(betNumber) || betNumber < 0 || betNumber >= 100) {
+        log.value += `Aposta inválida! Insira um número entre 0 e 99.\n`;
+        return;
+    }
 
-  
-    if (bet > numbertofind) {
-        alert('O número é menor.');
-    } else if (bet < numbertofind) {
-        alert('O número é maior.');
+    attempts++;
+
+    
+    if (betNumber > numbertofind) {
+        log.value += `Tentativa ${attempts}: ${betNumber} é maior\n`;
+    } else if (betNumber < numbertofind) {
+        log.value += `Tentativa ${attempts}: ${betNumber} é menor\n`;
     } else {
-        alert('Você acertou em ' + attempts + ' tentativa(s)!'); 
-        attempts = 0; 
-        refresh(); 
+        log.value += `Tentativa ${attempts}: ${betNumber} acertou\n`;
+        log.value += `Você acertou em ${attempts} tentativa(s)!\n`;
+        document.getElementById('bet').disabled = true; 
+        document.getElementById('submit-button').disabled = true; 
+        numbertofind = null; 
     }
 
     document.getElementById('bet').value = ''; 
 }
 
 
-refresh();
+function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+        verifyNumber(); 
+    }
+}
